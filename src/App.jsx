abfca25cf86a3a0d6f85be290881549516169484
Yash Chaure <142717959+yashchaure212@users.noreply.github.com>
2023-12-20 20:27:23 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editId, setEditId] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(editId) {
+      const editTodo = todos.find((i) => i.id === editId);
+      const updatedTodos = todos.map((t) => 
+      t.id === editTodo.id ? (t={id:t.id, todo}) : {id:t.id, todo: t.todo} )
+      setTodos(updatedTodos)
+      setEditId(0)
+      setTodo("")
+      return;
+    }
+
+    if(todo !== ""){
+      setTodos([{id:`${todo} - ${Date.now()}` , todo},...todos])
+      setTodo("");
+    }
+  }
+
+  const handleDelete = (id) => {
+    const deleteTodo = todos.filter((to) => to.id !== id)
+    setTodos([...deleteTodo]);
+  }
+
+  const handleEdit = (id) => {
+    const editTodo = todos.find((i) => i.id === id)
+    setTodo(editTodo.todo)
+    setEditId(id)
+  }
+  return (
+    
+    <div className="App">
+      <div className="container">
+        <h1>TODOS LIST APP</h1>
+        <form className='todoform' onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            value={todo} 
+            onChange={(e) => setTodo(e.target.value)}/>
+          <button type='submit'>{editId ? "Edit" : "go"}</button>
+        </form>
+        <ul className='allTodos'>
+          {todos.map((t) => (
+            <li className='SingleTodo'>
+            <span className='todoText' key={t.id}>{t.todo}</span>
+            <button onClick={() => handleEdit(t.id)}>edit</button>
+            <button onClick={() => handleDelete(t.id)}>delete</button>
+          </li>
+
+          ))}
+          
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
